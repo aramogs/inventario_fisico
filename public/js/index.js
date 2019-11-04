ebf7049e3775c844d524f6643f77f31ed203e1dd
aramogs <aramogs@users.noreply.github.com>
2019-11-04 08:07:09 -0700

    cambios en footer y header

--- public/js/index.js
+++ public/js/index.js
@@ -1,55 +1,67 @@
-
 // A $( document ).ready() block.
-$( document ).ready(function() {
-      
-          $('#myTable').dataTable( {
-            "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-          } );
+$(document).ready(function () {
 
-// Ocultar boton de atenter si ya esta atendida
-          if($('#status_cerrar2').text()=='Atendida'){
-          $('#atender').hide()
-          }
 
-});
+  $('#myTable').dataTable( {
+    "pageLength" : 7,
+    "ordering": false,
+    "bPaginate": true,
+    "bLengthChange": false,
+    "bFilter": false,
+    "bInfo": false,
+    "bAutoWidth": false,
+    responsive: true,
+    rowGroup: {
+        dataSrc: 'group'
+    },
+    rowReorder: true,
+    select: true,
+    autoFill: true
+  } );
 
-$("#menu-toggle").click(function (e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
+  $('#classTable').dataTable( {
+    "pageLength" : 10,
+    "ordering": true,
+    "bPaginate": true,
+    "bLengthChange": false,
+    "bFilter": true,
+    "bInfo": true,
+    "bAutoWidth": true,
+    responsive: true,
+    rowGroup: {
+        dataSrc: 'group'
+    },
+    rowReorder: true,
+    select: true,
+    autoFill: true
+  } );
 });
 
 var timer;
 
 function updateClock() {
-    $('.date_start_time').each(function() {
-        var startDateTime = new Date( $(this).attr('data-date-time') );
-        startStamp = startDateTime.getTime();
-        newDate = new Date();
-        newStamp = newDate.getTime();
-        var diff = Math.round((newStamp - startStamp) / 1000);
+  $('.date_start_time').each(function () {
+    var startDateTime = new Date($(this).attr('data-date-time'));
+    startStamp = startDateTime.getTime();
+    newDate = new Date();
+    newStamp = newDate.getTime();
+    var diff = Math.round((newStamp - startStamp) / 1000);
 
-        var d = Math.floor(diff / (24 * 60 * 60));
+    var d = Math.floor(diff / (24 * 60 * 60));
 
-        diff = diff - (d * 24 * 60 * 60);
-        var h = Math.floor(diff / (60 * 60));
-        diff = diff - (h * 60 * 60);
-        var m = Math.floor(diff / (60));
-        diff = diff - (m * 60);
-        var s = diff;
+    diff = diff - (d * 24 * 60 * 60);
+    var h = Math.floor(diff / (60 * 60));
+    diff = diff - (h * 60 * 60);
+    var m = Math.floor(diff / (60));
+    diff = diff - (m * 60);
+    var s = diff;
 
-        $(this).parent().find("div.time-elapsed").html(d + " dia(s), " + h + ":" + m + ":" + s);
-    });
+    $(this).parent().find("div.time-elapsed").html(d + " dia(s), " + h + ":" + m + ":" + s);
+  });
 }
 
 setInterval(updateClock, 1000);
 
-$('#escalamientoButton').click(function() {
+$('#escalamientoButton').click(function () {
   $("alert").show();
 })
-
-
-
-
-
-
-
